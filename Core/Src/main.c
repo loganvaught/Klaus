9e51359afea538674c7fcb4e82c3529768ec578f
Logan Vaught <loganvaught.r@gmail.com>
2026-06-03 15:01:59 -0700

    Added metronome buzzing with timer, adjusts with tempo from rotary encoder.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,6 +47,8 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
+TIM_HandleTypeDef htim2;
+
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
@@ -58,6 +60,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -186,12 +189,15 @@ void haptic_play(uint8_t num) {
 		return;
 	}
 
+	// Check if go bit is 0
+	if (haptic_read_byte(0x0C) != 0) return;
+
 	haptic_write_byte(0x04, num);
 
 	// Set GO bit
 	haptic_write_byte(0x0C, 0x01);
 
-	// Poll the go-bit to detect when vibration is completed
+	/* Poll the go-bit to detect when vibration is completed
 	uint8_t finished = 0;
 	uint8_t checks = 0;
 	uint8_t max_checks = 100;
@@ -200,6 +206,7 @@ void haptic_play(uint8_t num) {
 		if (haptic_read_byte(0x0C) == 0x00) finished = 1;
 		checks ++;
 	}
+	*/
 }
 
 uint8_t encoder_last_state = 0;
@@ -221,6 +228,16 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	if (GPIO_Pin == ENCODER_A_Pin || GPIO_Pin == ENCODER_B_Pin) {
 		// Encoder A pin
 		encoder_change = 1;
+	}
+}
+
+// Timer for generating metronome beats
+
+uint8_t metronome_beat = 0;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	if (htim->Instance == TIM2) {
+		// Timer 2 reset
+		metronome_beat = 1;
 	}
 }
 
@@ -257,10 +274,14 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
   printf("Working!\r\n");
   haptic_init();
+
+  // Start timer for generating metronome beats
+  HAL_TIM_Base_Start_IT(&htim2);
 
   /* CODE TO CYCLE THROUGH ALL THE VIBRATION WAVEFORMS
   for (uint8_t i = 1; i < 124; i ++) {
@@ -278,6 +299,7 @@ int main(void)
 
   while (1)
   {
+	  // ENCODER CODE
 	  if (encoder_change == 2) { // First time initializing encoder variables
 		  encoder_change = 0;
 		  encoder_state = (HAL_GPIO_ReadPin(ENCODER_A_GPIO_Port, ENCODER_A_Pin) << 1) | HAL_GPIO_ReadPin(ENCODER_B_GPIO_Port, ENCODER_B_Pin);
@@ -327,12 +349,24 @@ int main(void)
 				  tempo = new_tempo;
 			  }
 
-			  printf("New tempo: %d\r\n", tempo);
+			  // Calculate new counter period for timer with new tempo
+			  uint16_t new_period = 600000 / tempo;
+			  printf("New period: %d. New tempo: %d\r\n", new_period, tempo);
+			  __HAL_TIM_SET_COUNTER(&htim2, 0);
+			  __HAL_TIM_SET_AUTORELOAD(&htim2, new_period - 1);
 		  }
 
 		  encoder_last_state = encoder_state;
 		  encoder_change = 0;
 
+	  }
+
+	  // TIMER CODE
+
+	  if (metronome_beat) {
+		  // Play vibration waveform for beat
+		  haptic_play(1);
+		  metronome_beat = 0;
 	  }
 
     /* USER CODE END WHILE */
@@ -359,10 +393,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 12;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -372,12 +410,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
@@ -414,6 +452,51 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 10000 - 1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 10000 - 1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
 
 }
 
@@ -463,6 +546,7 @@ static void MX_GPIO_Init(void)
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
